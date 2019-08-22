f645abe3d02ac40276d8e2f952d2e791d777d32b
nthoms9 <54373687+nthoms9@users.noreply.github.com>
2019-08-22 14:29:11 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,15 @@
+/*
+Nick Thoms
+C++ 2019
+Due: 08/29/19
+Lab 1 "Hello World"
+Compile first project and upload to github 
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World! Purple";
+	return 0;
+}
